2d60b0863b8d05c100f3b496c38e50f45d9d2799
rohithanc <49763600+rohithanc@users.noreply.github.com>
2020-11-09 19:22:49 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
 
 ls -la /var/lib/jenkins/.ssh/
 
-ssh-add /var/lib/jenkins/.ssh/id_rsa
-ssh -i /var/lib/jenkins/.ssh/id_rsa rohithan9615@35.192.34.70 << EOF
+ssh -tt -i /var/lib/jenkins/.ssh/id_rsa rohithan9615@35.192.34.70 << EOF
 if ! [ $(docker network ls | grep duo-task-network) ]; then
   sudo docker network create duo-task-network
 fi
